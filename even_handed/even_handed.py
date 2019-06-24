4f5da6d832f977fa5b5e6c259cab76e8abd40ee6
Sebastian Lee <sjrl423@gmail.com>
2019-06-24 13:44:04 -0700

    PEP8 stuff

--- even_handed/even_handed.py
+++ even_handed/even_handed.py
@@ -34,46 +34,48 @@ def calculate_overlap_metric(mol1, mol2, sub_a_even_handed):
 
     # Calculate S1^1/2
     s_eig1, s_vec1 = np.linalg.eig(mol1.s_mat)
-    tmp_s_sqrt_mat1 = np.zeros([mol1.nao,mol1.nao])
+    tmp_s_sqrt_mat1 = np.zeros([mol1.nao, mol1.nao])
     for i in range(mol1.nao):
-        tmp_s_sqrt_mat1[:,i] = s_vec1[:,i] * np.sqrt(s_eig1[i])
+        tmp_s_sqrt_mat1[:, i] = s_vec1[:, i] * np.sqrt(s_eig1[i])
     s_sqrt_mat1 = np.matmul(tmp_s_sqrt_mat1, s_vec1.T)
 
     # Calculate S2^1/2
     s_eig2, s_vec2 = np.linalg.eig(mol2.s_mat)
-    tmp_s_sqrt_mat2 = np.zeros([mol2.nao,mol2.nao])
+    tmp_s_sqrt_mat2 = np.zeros([mol2.nao, mol2.nao])
     for i in range(mol2.nao):
-        tmp_s_sqrt_mat2[:,i] = s_vec2[:,i] * np.sqrt(s_eig2[i])
+        tmp_s_sqrt_mat2[:, i] = s_vec2[:, i] * np.sqrt(s_eig2[i])
     s_sqrt_mat2 = np.matmul(tmp_s_sqrt_mat2, s_vec2.T)
 
     # Calculate S^k,k+1 matrix
-    if mol1.nao == mol2.nao:
-        # Original way (from J. Chem. Phys. 2018, 149 (14), 144101.)
-        s12 = np.matmul(s_sqrt_mat1, s_sqrt_mat2)
-    elif mol1.nao < mol2.nao:
+    if mol1.nao < mol2.nao:
         # s12 is square with dimensions mol1.nao x mol1.nao
-        s12 = np.matmul(s_sqrt_mat1, s_sqrt_mat2[mol1.nao:,mol1.nao:])
+        s12 = np.matmul(s_sqrt_mat1, s_sqrt_mat2[mol1.nao:, mol1.nao:])
     elif mol1.nao > mol2.nao:
         # s12 is square with dimensions mol2.nao x mol2.nao
-        s12 = np.matmul(s_sqrt_mat1[mol2.nao:,mol2.nao:], s_sqrt_mat2)
+        s12 = np.matmul(s_sqrt_mat1[mol2.nao:, mol2.nao:], s_sqrt_mat2)
+    # mol1.nao == mol2.nao case
+    else:
+        # Original way (from J. Chem. Phys. 2018, 149 (14), 144101.)
+        s12 = np.matmul(s_sqrt_mat1, s_sqrt_mat2)
 
     # Calculate L^k * S^k,k+1 * L^k+1 matrix
-    if mol1.nao == mol2.nao:
-        # Original way (from J. Chem. Phys. 2018, 149 (14), 144101.)
-        # s12, mol1.lmo_mat and mol2.lmo_mat are square
-        # lmo1_s12_lmo2 is square nocc1 x nocc2 b/c nocc1 == nocc2
-        s12_lmo2 = np.dot(s12, mol2.lmo_mat)
-        lmo1_s12_lmo2 = np.dot(mol1.lmo_mat.T, s12_lmo2)
-    elif mol1.nao < mol2.nao:
+    if mol1.nao < mol2.nao:
         # s12, and mol1.lmo_mat is square
         # mol2.lmo_mat is rectangular (AO dimension is reduced to mol1.nao)
         # lmo1_s12_lmo2 is rectangular nocc1 x nocc2
-        s12_lmo2 = np.dot(s12, mol2.lmo_mat[mol1.nao:,:])
+        s12_lmo2 = np.dot(s12, mol2.lmo_mat[mol1.nao:, :])
         lmo1_s12_lmo2 = np.dot(mol1.lmo_mat.T, s12_lmo2)
     elif mol1.nao > mol2.nao:
         # s12, and mol2.lmo_mat is square
         # mol1.lmo_mat is rectangular (AO dimension is reduced to mol2.nao)
         # lmo1_s12_lmo2 is rectangular nocc1 x nocc2
+        s12_lmo2 = np.dot(s12, mol2.lmo_mat)
+        lmo1_s12_lmo2 = np.dot(mol1.lmo_mat.T, s12_lmo2)
+    # mol1.nao == mol2.nao case
+    else:
+        # Original way (from J. Chem. Phys. 2018, 149 (14), 144101.)
+        # s12, mol1.lmo_mat and mol2.lmo_mat are square
+        # lmo1_s12_lmo2 is square nocc1 x nocc2 b/c nocc1 == nocc2
         s12_lmo2 = np.dot(s12, mol2.lmo_mat)
         lmo1_s12_lmo2 = np.dot(mol1.lmo_mat.T, s12_lmo2)
 
@@ -84,18 +86,18 @@ def calculate_overlap_metric(mol1, mol2, sub_a_even_handed):
         # Original way (from J. Chem. Phys. 2018, 149 (14), 144101.)
         # Even handedly select subsystem A
         # Zero out rows that correspond to orbs in sub B in geom 1
-        for i in range(1,mol1.nocc+1):
+        for i in range(1, mol1.nocc+1):
             if i not in mol1.sub_a_indices:
-                tmp_overlap_metric[i-1,:] = 0.0
+                tmp_overlap_metric[i-1, :] = 0.0
     else:
         # Even handedly select subsystem B
         # Zero out rows that correspond to orbs in sub A in geom 1
-        for i in range(1,mol1.nocc+1):
+        for i in range(1, mol1.nocc+1):
             if i not in mol1.sub_b_indices:
-                tmp_overlap_metric[i-1,:] = 0.0
+                tmp_overlap_metric[i-1, :] = 0.0
 
     # Compress down to 1-dimension where rows are compressed and added
-    overlap_metric = np.sum(tmp_overlap_metric, axis = 0)
+    overlap_metric = np.sum(tmp_overlap_metric, axis=0)
     return overlap_metric[:mol2.nocc]
 
 
@@ -120,11 +122,11 @@ def even_handed(reaction_coord, sub_a_even_handed=True):
     print('Before even-handed embedding')
     print('Subsystem A orbitals')
     for item in reaction_coord:
-        print(str(item.root.split('/')[-1]) + ': ' + str(len(item.sub_a_indices)) + ' orbitals' )
+        print(str(item.root.split('/')[-1]) + ': ' + str(len(item.sub_a_indices)) + ' orbitals')
 
     print('Subsystem B orbitals')
     for item in reaction_coord:
-        print(str(item.root.split('/')[-1]) + ': ' + str(len(item.sub_b_indices)) + ' orbitals' )
+        print(str(item.root.split('/')[-1]) + ': ' + str(len(item.sub_b_indices)) + ' orbitals')
 
     if len(reaction_coord) == 1:
         raise RuntimeError('Need more than one coordinate to perform even-handed embedding.')
@@ -183,16 +185,14 @@ def even_handed(reaction_coord, sub_a_even_handed=True):
             even_handed_set1 = set(add1_sorted_indices1[-1*nocc_a2:])
             sub_a_indices1 = reaction_coord[i].sub_a_indices
             updated_sub_a_indices1 = sub_a_indices1.union(even_handed_set1)
-            updated_sub_b_indices1 = set([x for x in range(1,nocc1+1) if x not in updated_sub_a_indices1])
-            # updated_sub_b_indices1 = reaction_coord[i].sub_b_indices - updated_sub_a_indices1
+            updated_sub_b_indices1 = set([x for x in range(1, nocc1+1) if x not in updated_sub_a_indices1])
         else:
             # Select the nocc_b2 largest overlap_metric1 values for even_handed_set1
             nocc_b2 = reaction_coord[i+1].nocc_b
             even_handed_set1 = set(add1_sorted_indices1[-1*nocc_b2:])
             sub_b_indices1 = reaction_coord[i].sub_b_indices
             updated_sub_b_indices1 = sub_b_indices1.intersection(even_handed_set1)
-            updated_sub_a_indices1 = set([x for x in range(1,nocc1+1) if x not in updated_sub_b_indices1])
-            # updated_sub_a_indices1 = reaction_coord[i].sub_a_indices - updated_sub_b_indices1
+            updated_sub_a_indices1 = set([x for x in range(1, nocc1+1) if x not in updated_sub_b_indices1])
 
         # Update reaction_coord info for i
         reaction_coord[i].nocc_a = len(updated_sub_a_indices1)
@@ -217,11 +217,11 @@ def even_handed(reaction_coord, sub_a_even_handed=True):
     print('Even-handed Subsystem A Orbital Indices')
     print('Subsystem A indices')
     for item in reaction_coord:
-        print(item.root.split('/')[-1] + ': ' +str(list(item.sub_a_indices)))
+        print(item.root.split('/')[-1] + ': ' + str(list(item.sub_a_indices)))
 
     print('Subsystem B indices')
     for item in reaction_coord:
-        print(item.root.split('/')[-1] + ': ' +str(list(item.sub_b_indices)))
+        print(item.root.split('/')[-1] + ': ' + str(list(item.sub_b_indices)))
 
         # orb_fname = os.path.join(root,str(root.split('/')[-1]) + '_sub_a.txt')
         # with open(orb_fname,'w') as file1:
@@ -231,7 +231,7 @@ def even_handed(reaction_coord, sub_a_even_handed=True):
     print('')
     print('Overlap Metric for each Molecular Orbital')
     for item in reaction_coord:
-        print(item.root.split('/')[-1] + ': ' +str(list(item.overlap_metric)))
+        print(item.root.split('/')[-1] + ': ' + str(list(item.overlap_metric)))
 
         # overlap_fname = os.path.join(root,str(root.split('/')[-1]) + '_overlap_metric.txt')
         # with open(overlap_fname,'w') as file1:
